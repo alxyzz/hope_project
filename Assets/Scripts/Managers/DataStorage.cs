8b38517581e35122f6b96ccc0d31de8d859f9f91
alxyzz <93402865+alxyzz@users.noreply.github.com>
2022-05-27 16:08:22 +0200

    makes referencing stuff easier via a static function

---
+++ Assets/Scripts/Managers/DataStorage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// basically, I use DataStorage so I can call it, as a static function, from anywhere, to reference any of the instanced managers defined.
+///instead of using a singleton.
+/// </summary>
+public static class DataStorage
+{
+ 
+
+
+
+}
