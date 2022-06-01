2ed1007f2040adac7de7e8580339cf8dc99ebc55
alxyzz <93402865+alxyzz@users.noreply.github.com>
2022-06-01 14:45:14 +0200

    main menu WIP + separated the three levels

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    public void NewGame()
+    {
+
+    }
+
+
+
+    public void Continue()
+    {
+        //only one save slot (the last one)
+    }
+
+
+    public void Settings()
+    {
+
+    }
+    public void Exit()
+    {
+        //exit. save if required.
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
