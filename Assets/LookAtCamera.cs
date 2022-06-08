8ab021e71b7c40794d3e58fe4dc53a65ed95ca0b
alxyzz <93402865+alxyzz@users.noreply.github.com>
2022-06-08 15:34:48 +0200

    adds main char + makes it flip on move

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    //check where we just moved compared to last location
+    private SpriteRenderer sprenderer;
+    private float xloc;
+    // Update is called once per frame
+
+
+    private void Start()
+    {
+        sprenderer = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, 5f, transform.eulerAngles.z);
+        if (transform.position.x > xloc)
+        {//moved left
+            sprenderer.flipX = false;
+        }
+        else if (transform.position.x < xloc)
+        {//moved right
+            sprenderer.flipX = true;
+        }
+
+
+
+        xloc = transform.position.x;
+    }
+}
