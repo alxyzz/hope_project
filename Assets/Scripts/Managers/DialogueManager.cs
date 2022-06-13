872ec3b5d60cb1a0f415d89349a47dd803625234
alxyzz <93402865+alxyzz@users.noreply.github.com>
2022-06-13 14:04:16 +0200

    adds another self

--- Assets/Scripts/Managers/DialogueManager.cs
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -5,10 +5,19 @@ using Fungus;
 
 public class DialogueManager : MonoBehaviour
 {
-    
+
 
 
     // this is basically how we talk to NPCs. Fungus has it's own variables i believe to decide the dialogue so this is just so we can call these functions via a UnityEvent in the character's entity
+
+
+    public void PositiveSelfTalk()
+    {
+        Debug.Log("we talked");
+
+
+
+    }
 
     public void TalkJulia()
     {
