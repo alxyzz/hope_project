e4d13b639437f2e171bdc516c7ba9ba4d983ceed
alxyzz <93402865+alxyzz@users.noreply.github.com>
2022-06-13 15:16:25 +0200

    test dialogue, just click your self

--- Assets/Scripts/Managers/DialogueManager.cs
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -6,7 +6,7 @@ using Fungus;
 public class DialogueManager : MonoBehaviour
 {
 
-
+    public Flowchart selfFlowchart;
 
     // this is basically how we talk to NPCs. Fungus has it's own variables i believe to decide the dialogue so this is just so we can call these functions via a UnityEvent in the character's entity
 
@@ -14,7 +14,7 @@ public class DialogueManager : MonoBehaviour
     public void PositiveSelfTalk()
     {
         Debug.Log("we talked");
-
+        selfFlowchart.ExecuteBlock("player clicked self");
 
 
     }
