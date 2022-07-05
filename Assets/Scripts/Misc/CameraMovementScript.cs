984565a52b39f6ae1b246ee5a06416807d3da205
alxyzz <93402865+alxyzz@users.noreply.github.com>
2022-07-06 01:22:45 +0200

    camera pans front-back as well so it looks better in the hallway

--- Assets/Scripts/Misc/CameraMovementScript.cs
+++ Assets/Scripts/Misc/CameraMovementScript.cs
@@ -11,20 +11,28 @@ public class CameraMovementScript : MonoBehaviour
     [SerializeField]
     private float movementStep;
 
+    private float desiredDistanceFromPlayer;
+    private Camera mainCam;
+    private void Start()
+    {
+        mainCam = Camera.main;//so we only call it once
+        desiredDistanceFromPlayer = Vector3.Distance(transform.position,player.transform.position);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (Mathf.Abs(player.transform.position.x - transform.position.x) > maximumHorizontalDistanceFromPlayer)
         {
             Vector3 b = Vector3.MoveTowards(transform.position, player.transform.position, movementStep);
             transform.position = new Vector3(b.x, transform.position.y, transform.position.z);
         }
-
+        float dist = player.transform.position.z - mainCam.transform.position.z;
+        if (Mathf.Abs(dist) != desiredDistanceFromPlayer)
+        {
+            Vector3 b = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + desiredDistanceFromPlayer - 0.1f), 0.03f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, b.z ); //magic number my beloved
+        }
     }
-
-
-
 }
