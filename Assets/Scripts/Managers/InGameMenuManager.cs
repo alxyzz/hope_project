9f9c853d27a552cf53a18bd919b7788f16b2d498
ritablob <106591191+ritablob@users.noreply.github.com>
2022-07-11 21:04:56 +0200

    Pause menu wip + fixed audio looping issue

--- Assets/Scripts/Managers/InGameMenuManager.cs
+++ Assets/Scripts/Managers/InGameMenuManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class InGameMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject setingsReference;
+    public GameObject areYouSureReference;
+    public GameObject pauseMenuReference;
+    
+    public void ResumeButton() // goes back to the game
     {
-        
+        pauseMenuReference.SetActive(false);
+        setingsReference.SetActive(false);
+        areYouSureReference.SetActive(false);
+        Time.timeScale = 1;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SettingsButton() // audio settings ? if i have time
     {
-        
+        setingsReference.SetActive(true);
+        pauseMenuReference.SetActive(false);
+    }
+    public void QuitButtonOne() // turns on prompt "are you sure?"
+    {
+        areYouSureReference.SetActive(true);
+    }
+    public void QuitButtonTwo() // quits
+    {
+        Application.Quit();
     }
 }
