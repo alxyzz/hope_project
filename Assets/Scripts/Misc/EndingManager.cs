8cb996be2b64e646e7e55d4f66bbe749d0af902c
ritablob <106591191+ritablob@users.noreply.github.com>
2022-07-13 05:19:39 +0200

    Good ending

---
+++ Assets/Scripts/Misc/EndingManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingManager : MonoBehaviour
+{
+
+    public GameObject creditsGO;
+    public Transform creditsText;
+    public float creditRollSpeed = 2f;
+    public bool creditsHaveStarted = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (creditsHaveStarted)
+        {
+            creditsText.position = new Vector3(creditsText.position.x, creditsText.position.y + creditRollSpeed);
+            if (creditsText.position.y > 4500)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    public void RollCredits()
+    {
+        creditsGO.SetActive(true);
+        creditsHaveStarted = true;
+    }
+}
